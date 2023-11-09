4ca93595dec0c5e57dc69e6fe90eec5342ddf25b
rwijtvliet <rwijtvliet@gmail.com>
2023-11-09 14:21:50 +0100

    added rg and fd to utils installation

--- utils/install.sh
+++ utils/install.sh
@@ -3,34 +3,32 @@
 source ../shared.sh
 
 case "$OS" in
-  "linux" )
-    sudo apt install npm snapd
+    "linux" )
+        sudo apt install npm snapd
 
-    sudo apt update
-    sudo apt upgrade
-    ;;
+        sudo apt update
+        sudo apt upgrade
+        ;;
 
-  "windows" )
-    scoop install wget   
-    scoop install npm
-    scoop install nodejs
+    "windows" )
+        scoop install wget
+        scoop install npm
+        scoop install nodejs
 
-    scoop update *
-    ;;
+        scoop update *
+        ;;
 
-  "macos" )
-    info "Installing gnu versions"
-    brew install coreutils findutils gnu-tar gnu-sed gawk gnutls gnu-indent gnu-getopt grep
-    PATH="/opt/homebrew/opt/coreutils/libexec/gnubin:$PATH" # also in .bashrc
-    info "Installing java"
-    brew install java
-    brew install openjdk
-    todo "installed java - probably won't work anyway. "
-    brew install npm
-    brew install wget
-    brew install nodejs
-    ;;
-    
-  * )
-    ;;
+    "macos" )
+        info "Installing gnu versions"
+        brew install coreutils findutils gnu-tar gnu-sed gawk gnutls gnu-indent gnu-getopt grep
+        PATH="/opt/homebrew/opt/coreutils/libexec/gnubin:$PATH" # also in .bashrc
+        info "Installing java"
+        brew install java
+        brew install openjdk
+        todo "installed java - but probably won't work anyway. "
+        brew install npm wget nodejs rg fd
+        ;;
+
+    * )
+        ;;
 esac
