122d7280ce8617bef449f6b06d39248f8a8ce100
rwijtvliet <rwijtvliet@gmail.com>
2023-12-19 23:17:19 +0100

    directory structure for voyager

--- keyboard/install.sh
+++ keyboard/install.sh
@@ -2,9 +2,13 @@
 
 source ../shared.sh
 
-ROOT="syncNone/Software/qmk_firmware"
-ERGODOX="keyboards/ergodox_ez/keymaps/rwijtvliet"
-ATREUS="keyboards/keyboardio/atreus/keymaps/rwijtvliet"
+ROOT1="syncNone/Software/qmk_firmware"
+ROOT2="syncNone/Software/qmk_firmware_zsa"
+
+ERGODOX="$ROOT1/keyboards/ergodox_ez/keymaps/rwijtvliet"
+ATREUS="$ROOT1/keyboards/keyboardio/atreus/keymaps/rwijtvliet"
+VOYAGER="$ROOT2/keyboards/voyager/keymaps/rwijtvliet"
+
 LAST_SUCCESSFUL_QMK_TAG="0.22.10"  # see https://github.com/qmk/qmk_firmware/tags
 
 
@@ -12,49 +16,74 @@ setup_and_config() {
   home=$1
   
   info "Running setup with LATEST qmk version. See below if this is not working with existing files."
-  qmk setup --home "$home"/$ROOT
+  qmk setup --home "$home"/$ROOT1
   
   info "Linking config for atreus"
-  rm -rf   "$home"/"$ROOT"/"$ATREUS"
-  mkdir -p "$home"/"$ROOT"/"$ATREUS"
-  link_public_resource "./qmk/common/keymap_partial.c"     "$home"/"$ROOT"/"$ATREUS"/keymap_partial.c
-  link_public_resource "./qmk/common/dicts.def"            "$home"/"$ROOT"/"$ATREUS"/dicts.def
-  link_public_resource "./qmk/common/config.h"             "$home"/"$ROOT"/"$ATREUS"/config.h
-  link_public_resource "./qmk/keyboardio_atreus/atreus.c"  "$home"/"$ROOT"/"$ATREUS"/atreus.c
-  link_public_resource "./qmk/keyboardio_atreus/keymap.c"  "$home"/"$ROOT"/"$ATREUS"/keymap.c
-  link_public_resource "./qmk/keyboardio_atreus/rules.mk"  "$home"/"$ROOT"/"$ATREUS"/rules.mk
-  link_secret_resource "qmk/common/combos.def"             "$home"/"$ROOT"/"$ATREUS"/combos.def
+  rm -rf   "$home"/"$ATREUS"
+  mkdir -p "$home"/"$ATREUS"
+  link_public_resource "./qmk/common/keymap_partial.c"     "$home"/"$ATREUS"/keymap_partial.c
+  link_public_resource "./qmk/common/dicts.def"            "$home"/"$ATREUS"/dicts.def
+  link_public_resource "./qmk/common/config.h"             "$home"/"$ATREUS"/config.h
+  link_public_resource "./qmk/keyboardio_atreus/atreus.c"  "$home"/"$ATREUS"/atreus.c
+  link_public_resource "./qmk/keyboardio_atreus/keymap.c"  "$home"/"$ATREUS"/keymap.c
+  link_public_resource "./qmk/keyboardio_atreus/rules.mk"  "$home"/"$ATREUS"/rules.mk
+  link_secret_resource "qmk/common/combos.def"             "$home"/"$ATREUS"/combos.def
     
   info "Linking config for ergodox_ez"
-  rm -rf   "$home"/"$ROOT"/"$ERGODOX"
-  mkdir -p "$home"/"$ROOT"/"$ERGODOX"
-  link_public_resource "./qmk/common/keymap_partial.c"     "$home"/"$ROOT"/"$ERGODOX"/keymap_partial.c
-  link_public_resource "./qmk/common/dicts.def"            "$home"/"$ROOT"/"$ERGODOX"/dicts.def
-  link_public_resource "./qmk/common/config.h"             "$home"/"$ROOT"/"$ERGODOX"/config.h
-  link_public_resource "./qmk/ergodox_ez_glow/ergodox.c"   "$home"/"$ROOT"/"$ERGODOX"/ergodox.c
-  link_public_resource "./qmk/ergodox_ez_glow/keymap.c"    "$home"/"$ROOT"/"$ERGODOX"/keymap.c
-  link_public_resource "./qmk/ergodox_ez_glow/rules.mk"    "$home"/"$ROOT"/"$ERGODOX"/rules.mk
-  link_secret_resource "qmk/common/combos.def"             "$home"/"$ROOT"/"$ERGODOX"/combos.def
+  rm -rf   "$home"/"$ERGODOX"
+  mkdir -p "$home"/"$ERGODOX"
+  link_public_resource "./qmk/common/keymap_partial.c"     "$home"/"$ERGODOX"/keymap_partial.c
+  link_public_resource "./qmk/common/dicts.def"            "$home"/"$ERGODOX"/dicts.def
+  link_public_resource "./qmk/common/config.h"             "$home"/"$ERGODOX"/config.h
+  link_public_resource "./qmk/ergodox_ez_glow/ergodox.c"   "$home"/"$ERGODOX"/ergodox.c
+  link_public_resource "./qmk/ergodox_ez_glow/keymap.c"    "$home"/"$ERGODOX"/keymap.c
+  link_public_resource "./qmk/ergodox_ez_glow/rules.mk"    "$home"/"$ERGODOX"/rules.mk
+  link_secret_resource "qmk/common/combos.def"             "$home"/"$ERGODOX"/combos.def
   
   info "run 'qmk list-keyboards' to see all supported keyboards. Here are the first 20:"
   qmk list-keyboards 2>&1 | head -n 20
   qmk config user.keymap=rwijtvliet
   todo "To compile the ergodox, use 'qmk compile -kb ergodox_ez/glow -km rwijtvliet"
-  todo "To compile the atreus, replace 'ergodox_ez/glow' with 'keyboardio/atreus'"
+  todo "To compile the atreus, use 'qmk compile -kb keyboardio/atreus -km rwijtvliet"
   todo "To flash the compiled file to the keyboard, replace 'compile' with 'flash'"
   info "Let's try compiling our ergodox keymap:"
   try "qmk compile -kb ergodox_ez/glow -km rwijtvliet"
   info "Let's try compiling our atreus keymap:"
   try "qmk compile -kb keyboardio/atreus -km rwijtvliet"
 
-  todo "If this was not successful, try setting up qmk using the branch that was used and working last time. Use the following command: 'qmk setup --home "$home"/$ROOT --branch $LAST_SUCCESSFUL_QMK_TAG' "
+  todo "If this was not successful, try setting up qmk using the branch that was used and working last time. Use the following command: 'qmk setup --home "$home"/$ROOT1 --branch $LAST_SUCCESSFUL_QMK_TAG' "
 }
 
+setup_and_config2() {
+  home=$1
+  
+  info "Installing ZSA fork of qmk."
+  git clone http://www.github.com/zsa/qmk_firmware "$home"/"$ROOT2"
+  
+  info "Linking config for voyager"
+  rm -rf   "$home"/"$VOYAGER"
+  mkdir -p "$home"/"$VOYAGER"
+  link_public_resource "./qmk/common/keymap_partial.c"     "$home"/"$VOYAGER"/keymap_partial.c
+  link_public_resource "./qmk/common/dicts.def"            "$home"/"$VOYAGER"/dicts.def
+  link_public_resource "./qmk/common/config.h"             "$home"/"$VOYAGER"/config.h
+  link_public_resource "./qmk/zsa_voyager/voyager.c"       "$home"/"$VOYAGER"/voyager.c
+  link_public_resource "./qmk/zsa_voyager/keymap.c"        "$home"/"$VOYAGER"/keymap.c
+  link_public_resource "./qmk/zsa_voyager/rules.mk"        "$home"/"$VOYAGER"/rules.mk
+  link_secret_resource "qmk/common/combos.def"             "$home"/"$VOYAGER"/combos.def
+  
+  todo "To compile the voyager, use 'qmk compile -kb voyager -km rwijtvliet"
+  todo "To flash the compiled file to the keyboard, replace 'compile' with 'flash'"
+  info "Let's try compiling our voyager keymap:"
+  try "cd $home/$ROOT2 && qmk compile -kb voyager -km rwijtvliet"
+
+  todo "If this was not successful, ask the guys at zsa what's wrong."
+}
 case "$OS" in
   "linux" )
     info "Installing app"
     python3 -m pip install --user qmk
     setup_and_config $HOME
+    setup_and_config2 $HOME
     ;;
 
   "windows" )
@@ -67,6 +96,7 @@ case "$OS" in
     info "Installing app (if not up-to-date, this will take several hours (!) on apple silicon)"
     brew install qmk/qmk/qmk
     setup_and_config $HOME
+    setup_and_config2 $HOME
     ;;
     
   * )
---
+++ keyboard/qmk/zsa_voyager/rules.mk
@@ -0,0 +1,13 @@
+# Set any rules.mk overrides for your specific keymap here.
+# See rules at https://docs.qmk.fm/#/config_options?id=the-rulesmk-file
+CONSOLE_ENABLE = no
+COMMAND_ENABLE = no
+MOUSEKEY_ENABLE = no
+ORYX_ENABLE = yes
+RGB_MATRIX_CUSTOM_KB = yes
+TAP_DANCE_ENABLE = yes
+SPACE_CADET_ENABLE = no
+CAPS_WORD_ENABLE = yes
+PROGRAM_CMD = $(call EXEC_DFU)
+DFU_ARGS = -d 3297:0791 -a 0 -s 0x08002000:leave
+SRC = matrix.c
