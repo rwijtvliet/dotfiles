c3aea8b608e2128986e1529dc97fa8f6ef4df473
rwijtvliet <rwijtvliet@gmail.com>
2024-04-16 20:16:18 +0200

    alacritty

--- neovim/user/mappings.lua
+++ neovim/user/mappings.lua
@@ -8,33 +8,24 @@ local is_available = utils.is_available
 -- automatically pick-up stored data by this setting.)
 local maps = {
   [""] = {
-    -- navigation
-    ["h"] = { "h", desc = "Move left" },
-    ["n"] = { "v:count == 0 || mode(1)[0:1] == 'no' ? 'j' : 'gj'", expr = true, desc = "Move down" },
-    ["t"] = { "v:count == 0 || mode(1)[0:1] == 'no' ? 'k' : 'gk'", expr = true, desc = "Move up" },
-    ["s"] = { "l", desc = "Move right" },
-    ["S"] = { "L", desc = "Move cursor to bottom of screen" },
-    -- search
-    ["l"] = { "n", desc = "Next search result" },
-    ["L"] = { "N", desc = "Prev search result" },
-    -- leap
-    ["m"] = { "<Plug>(leap-forward-to)", desc = "Leap forward to match" },
-    ["M"] = { "<Plug>(leap-backward-to)", desc = "Leap backward to match" },
-    ["gm"] = { "<Plug>(leap-from-window)", desc = "Leap to match in other window" },
-    -- move begining/end of line
-    ["B"] = { "^", desc = "Move to beginning of line" },
-    ["E"] = { "$", desc = "Move to end of line" },
-    -- set mark
-    ["<A-m>"] = { "m", desc = "Set marker" },
-    -- move till word
-    ["k"] = { "t", noremap = true, desc = "Till (forward)" }, -- expr = false needed to override astrovim mapping for k
-    ["K"] = { "T", noremap = true, desc = "Till (backward)" },
+    -- -- leap
+    -- ["m"] = { "<Plug>(leap-forward-to)", desc = "Leap forward to match" },
+    -- ["M"] = { "<Plug>(leap-backward-to)", desc = "Leap backward to match" },
+    -- ["gm"] = { "<Plug>(leap-from-window)", desc = "Leap to match in other window" },
+    -- -- move begining/end of line
+    -- ["B"] = { "^", desc = "Move to beginning of line" },
+    -- ["E"] = { "$", desc = "Move to end of line" },
+    -- -- set mark
+    -- ["<A-m>"] = { "m", desc = "Set marker" },
+    -- -- move till word
+    -- ["k"] = { "t", noremap = true, desc = "Till (forward)" }, -- expr = false needed to override astrovim mapping for k
+    -- ["K"] = { "T", noremap = true, desc = "Till (backward)" },
   },
 
   -- first key is the mode
   n = {
     -- substitute
-    ["j"] = { "s", expr = false, noremap = true, desc = "Substitute character" }, -- expr = false needed to override astrovim mapping for j
+    -- ["j"] = { "s", expr = false, noremap = true, desc = "Substitute character" }, -- expr = false needed to override astrovim mapping for j
     -- REPL with iron (see :h iron-commands for all available commands)
     ["<leader>s"] = { desc = get_icon("Run", 1, true) .. "Run" },
     ["<leader>so"] = { "<cmd>IronRepl<cr>", desc = "Open/toggle REPL" },
@@ -154,28 +145,3 @@ local maps = {
     ["<leader>sv"] = { function() require("iron.core").send_visual() end, desc = "Run selection" },
   },
 }
--- Move lines.
-maps.n["<A-t>"] = { "<cmd>m .-2<CR>==", desc = "Move line up" }
-maps.n["<A-n>"] = { "<cmd>m .+1<CR>==", desc = "Move line down" }
-maps.i["<A-t>"] = { "<Esc><cmd>m .-2<CR>==gi", desc = "Move line up" }
-maps.i["<A-n>"] = { "<Esc><cmd>m .+1<CR>==gi", desc = "Move line down" }
-maps.v["<A-t>"] = { ":m .-2<CR>==", desc = "Move line(s) up" }
-maps.v["<A-n>"] = { ":m .+1<CR>==", desc = "Move line(s) down" }
-for _, mode in ipairs { "n", "i", "v" } do
-  maps[mode]["<A-Up>"] = maps[mode]["<A-t>"]
-  maps[mode]["<A-Down>"] = maps[mode]["<A-n>"]
-end
--- Move between splits.
-if is_available "smart-splits.nvim" then
-  maps.n["<C-h>"] = { function() require("smart-splits").move_cursor_left() end, desc = "Move to left split" }
-  maps.n["<C-n>"] = { function() require("smart-splits").move_cursor_down() end, desc = "Move to below split" }
-  maps.n["<C-t>"] = { function() require("smart-splits").move_cursor_up() end, desc = "Move to above split" }
-  maps.n["<C-s>"] = { function() require("smart-splits").move_cursor_right() end, desc = "Move to right split" }
-else
-  maps.n["<C-h>"] = { "<C-w>h", desc = "Move to left split" }
-  maps.n["<C-n>"] = { "<C-w>j", desc = "Move to below split" }
-  maps.n["<C-t>"] = { "<C-w>k", desc = "Move to above split" }
-  maps.n["<C-s>"] = { "<C-w>l", desc = "Move to right split" }
-end
-
-return maps
