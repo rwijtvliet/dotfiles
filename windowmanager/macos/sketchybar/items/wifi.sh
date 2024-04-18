fa8872a68f47f226a298e491ecf0d20a0c56acb1
rwijtvliet <rwijtvliet@gmail.com>
2024-04-19 01:04:29 +0200

    removed wifi from sketchybar

--- windowmanager/macos/sketchybar/items/wifi.sh
+++ windowmanager/macos/sketchybar/items/wifi.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
 wifi=(
-    icon=""
-    label.width=dynamic
-    padding_right=7
-    background.color="$TRANSPARENT"
-    script="$PLUGIN_DIR/wifi.sh"
+	icon=""
+	label.width=dynamic
+	padding_right=7
+	background.color="$TRANSPARENT"
+	script="$PLUGIN_DIR/wifi.sh"
 )
 
 sketchybar \
-    --add item wifi right \
-    --set wifi "${wifi[@]}" \
-    --subscribe wifi wifi_change mouse.clicked \
+	--add item wifi right \
+	--set wifi "${wifi[@]}" \
+	--subscribe wifi wifi_change mouse.clicked
--- windowmanager/macos/sketchybar/plugins/wifi.sh
+++ windowmanager/macos/sketchybar/plugins/wifi.sh
@@ -4,33 +4,36 @@ CONNECTED="􀙇"
 DISCONNECTED="􀙈"
 
 update() {
-    source "$CONFIG_DIR/shared.sh"
-    info="$(/System/Library/PrivateFrameworks/Apple80211.framework/Resources/airport -I | awk -F ' SSID: '  '/ SSID: / {print $2}')"
-    if [ -n "$info" ]; then
-        ip="$(ipconfig getifaddr en0)"
-        label="$info ($ip)"
-        icon="$CONNECTED"
-        color="$PRIMARY"
-    else
-        label="-"
-        icon="$DISCONNECTED"
-        color="$ALERT"
-    fi
-    sketchybar --set "$NAME" icon="$icon" label="$label" icon.color="$color"
+	source "$CONFIG_DIR/shared.sh"
+	# problem: deprecated
+	info="$(/System/Library/PrivateFrameworks/Apple80211.framework/Resources/airport -I | awk -F ' SSID: ' '/ SSID: / {print $2}')"
+	# problem: needs sudo
+	info="$(sudo wdutil info | grep ' SSID ' | awk -F ': ' '{print $2}')"
+	if [ -n "$info" ]; then
+		ip="$(ipconfig getifaddr en0)"
+		label="$info ($ip)"
+		icon="$CONNECTED"
+		color="$PRIMARY"
+	else
+		label="-"
+		icon="$DISCONNECTED"
+		color="$ALERT"
+	fi
+	sketchybar --set "$NAME" icon="$icon" label="$label" icon.color="$color"
 }
 
 click() {
-    current_width="$(sketchybar --query "$NAME" | jq -r .label.width)"
-    width=$([ "$current_width" -eq "0" ] && echo dynamic || echo 0)
+	current_width="$(sketchybar --query "$NAME" | jq -r .label.width)"
+	width=$([ "$current_width" -eq "0" ] && echo dynamic || echo 0)
 
-    sketchybar --animate sin 10 --set "$NAME" label.width="$width"
+	sketchybar --animate sin 10 --set "$NAME" label.width="$width"
 }
 
 case "$SENDER" in
-    "wifi_change")
-        update
-        ;;
-    "mouse.clicked")
-        click
-        ;;
+"wifi_change")
+	update
+	;;
+"mouse.clicked")
+	click
+	;;
 esac
