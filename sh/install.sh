3a9cf9b5983792988870d2504bd38ffcbac99caf
rwijtvliet <rwijtvliet@gmail.com>
2024-07-22 15:20:15 +0200

    moved bash and zsh to same folder

--- sh/install.sh
+++ sh/install.sh
@@ -3,36 +3,40 @@
 source ../shared.sh
 
 link_osunspecific_files() {
+  link_public_resource "./bashrc" "$HOME/.bashrc"
   link_public_resource "./zshrc" "$HOME/.zshrc"
-  link_public_resource "../bash/bash_aliases" "$HOME/.bash_aliases"
-  link_secret_resource "bash_secrets" "$HOME/.bash_secrets"
+  link_public_resource "./sh_aliases" "$HOME/.sh_aliases"
+  link_secret_resource "sh_secrets" "$HOME/.sh_secrets"
 }
 
 case "$OS" in
   "linux" )
     info "Linking config"
     link_osunspecific_files
-    link_public_resource "./bash_osspecific_linux" "$HOME/.bash_osspecific"
+    link_public_resource "./sh_osspecific_linux" "$HOME/.sh_osspecific"
     ;;
 
   "windows" )
     info "Linking config"
     link_osunspecific_files
-    link_public_resource "./bash_osspecific_windows" "$HOME/.bash_osspecific"
+    link_public_resource "./sh_osspecific_windows" "$HOME/.sh_osspecific"
     ;;
 
   "macos" )
-    info "Installing app"
+    info "Installing bash"
+    brew install bash
+    echo /opt/homebrew/bin/bash | sudo tee -a /etc/shells  # allow usage as shell
+    
+    info "Installing zsh"
     brew install zsh
-    PATH="/opt/homebrew/bin:$PATH" #line also included in bashrc
     echo /opt/homebrew/bin/zsh | sudo tee -a /etc/shells  # allow usage as shell
     chsh -s /opt/homebrew/bin/zsh  #make default
     
     info "Linking config"
     link_osunspecific_files
-    link_public_resource "../bash/bash_osspecific_macos" "$HOME/.bash_osspecific"
+    link_public_resource "./sh_osspecific_macos" "$HOME/.sh_osspecific"
 
-    info "Installing utilities"
+    info "Installing sh utilities"
     brew install powerlevel10k zsh-autosuggestions zsh-syntax-highlighting eza zoxide
     ;;
 
