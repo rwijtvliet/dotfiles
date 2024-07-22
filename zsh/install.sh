da7825e631afcf62f2913ee644bf87b7b65092db
rwijtvliet <rwijtvliet@gmail.com>
2024-07-22 14:59:32 +0200

    zsh

--- zsh/install.sh
+++ zsh/install.sh
@@ -3,8 +3,8 @@
 source ../shared.sh
 
 link_osunspecific_files() {
-  link_public_resource "./bashrc" "$HOME/.bashrc"
-  link_public_resource "./bash_aliases" "$HOME/.bash_aliases"
+  link_public_resource "./zshrc" "$HOME/.zshrc"
+  link_public_resource "../bash/bash_aliases" "$HOME/.bash_aliases"
   link_secret_resource "bash_secrets" "$HOME/.bash_secrets"
 }
 
@@ -23,17 +23,17 @@ case "$OS" in
 
   "macos" )
     info "Installing app"
-    brew install bash
+    brew install zsh
     PATH="/opt/homebrew/bin:$PATH" #line also included in bashrc
-    echo /opt/homebrew/bin/bash | sudo tee -a /etc/shells  # allow usage as shell
-    chsh -s /opt/homebrew/bin/bash  #make default
+    echo /opt/homebrew/bin/zsh | sudo tee -a /etc/shells  # allow usage as shell
+    chsh -s /opt/homebrew/bin/zsh  #make default
     
     info "Linking config"
     link_osunspecific_files
-    link_public_resource "./bash_osspecific_macos" "$HOME/.bash_osspecific"
+    link_public_resource "../bash/bash_osspecific_macos" "$HOME/.bash_osspecific"
 
-    info "Utilities"
-    brew install eza
+    info "Installing utilities"
+    brew install powerlevel10k zsh-autosuggestions zsh-syntax-highlighting eza zoxide
     ;;
 
   * )
