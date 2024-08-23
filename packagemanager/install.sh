e6280550bfa14381ff5417a6196c1656eaa7af79
rwijtvliet <rwijtvliet@gmail.com>
2024-08-23 13:25:47 +0200

    packagemanager into one

--- packagemanager/install.sh
+++ packagemanager/install.sh
@@ -8,19 +8,24 @@ case "$OS" in
     ;;
 
   "windows" )
-    # install
+
+    # Scoop
+    powershell "iwr -useb get.scoop.sh | iex"
+    powershell "Set-ExecutionPolicy RemoteSigned -scope CurrentUser" 
+    scoop update
+
+    # Choco
     policy=$(powershell Get-ExecutionPolicy)
     if [ $policy = "Restricted" ]; then
       echo $(powershell Set-ExecutionPolicy AllSigned)
     fi
-    
     powershell "Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))"
-
     choco update chocolatey
     ;;
 
   "macos" )
-    skip
+    
+    /usr/bin/env bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
     ;;
     
   * )
